6689fba41b23ee6aac914253ca2322d4eca2ec9e
Alecuu20CMRecords <165511607+Alecuu20CMRecords@users.noreply.github.com>
2026-01-28 14:34:03 +0200

    Add files via upload

---
+++ dc.js
@@ -0,0 +1,74 @@
+const slideRows = document.querySelectorAll('.slide-row');
+let currentIndex = 0;
+
+function showNextSlides() {
+    slideRows.forEach(row => row.style.display = 'none');
+    slideRows[currentIndex].style.display = 'flex';
+
+    currentIndex++;
+    if (currentIndex >= slideRows.length) {
+        currentIndex = 0;
+    }
+}
+
+showNextSlides();
+
+setInterval(showNextSlides, 8000);
+
+const form = document.getElementById('mcForm');
+const successMsg = document.getElementById('successMsg');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const node = document.getElementById('node').value;
+    const serverName = document.getElementById('serverName').value;
+    const colors = document.getElementById('colors').value;
+    const logo = document.getElementById('logo').value;
+    const discord = document.getElementById('discord').value;
+    const mcName = document.getElementById('mcName').value;
+
+    const params = {
+        email: email,
+        node: node,
+        serverName: serverName,
+        colors: colors,
+        logo: logo,
+        discord: discord,
+        mcName: mcName
+    };
+
+    emailjs.send('service_pyph6vw', 'template_3nzqaoo', params, 'O6vEGYohXD5pDr_ph')
+        .then(function() {
+            successMsg.style.opacity = 1;
+            successMsg.textContent = 'Înregistrare trimisă cu succes!';
+            form.reset();
+            setTimeout(() => { successMsg.style.opacity = 0; }, 4000);
+        }, function(error) {
+            alert('Eroare la trimitere: ' + JSON.stringify(error));
+        });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const popup = document.getElementById("popupMessage");
+    const closeBtn = document.getElementById("popupClose");
+    const progress = document.getElementById("popupProgress");
+
+    setTimeout(() => {
+        popup.classList.add("show");
+
+        progress.style.width = "0%";
+
+        setTimeout(() => {
+            popup.classList.remove("show");
+            popup.classList.add("hide");
+        }, 10000);
+
+    }, 2000); 
+
+    closeBtn.addEventListener("click", () => {
+        popup.classList.remove("show");
+        popup.classList.add("hide");
+    });
+});
